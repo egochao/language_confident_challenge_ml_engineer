ae972cd4ba39dd17ff945a572dabbdf41eac0969
egochao <johnoldmanworkspace@gmail.com>
2022-08-08 10:18:17 +0000

    check audio

--- utils/visualize_utils.py
+++ utils/visualize_utils.py
@@ -3,14 +3,28 @@ from os.path import isdir, join
 from pathlib import Path
 import pandas as pd
 
-# Math
 import numpy as np
 from scipy.fftpack import fft
 from scipy import signal
 from scipy.io import wavfile
 
 from sklearn.decomposition import PCA
+import torch
+import numpy as np
+from scipy.fftpack import fft
+from scipy import signal
+from scipy.io import wavfile
+from torchaudio.datasets import SPEECHCOMMANDS
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import IPython.display as ipd
+import librosa.display
+
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly.tools as tls
+import pandas as pd
 
 
 def log_specgram(audio, sample_rate, window_size=20,
@@ -24,3 +38,23 @@ def log_specgram(audio, sample_rate, window_size=20,
                                     noverlap=noverlap,
                                     detrend=False)
     return freqs, times, np.log(spec.T.astype(np.float32) + eps)
+
+
+def plot_audio(file_name):
+    sample_rate, samples = wavfile.read(file_name)
+    freqs, times, spectrogram = log_specgram(samples, sample_rate)
+
+    fig = plt.figure(figsize=(14, 8))
+    ax1 = fig.add_subplot(211)
+    ax1.set_title('Raw wave')
+    ax1.set_ylabel('Amplitude')
+    ax1.plot(samples)
+
+    ax2 = fig.add_subplot(212)
+    ax2.imshow(spectrogram.T, aspect='auto', origin='lower', 
+            extent=[times.min(), times.max(), freqs.min(), freqs.max()])
+    ax2.set_yticks(freqs[::16])
+    ax2.set_xticks(times[::16])
+    ax2.set_title('Spectrogram')
+    ax2.set_ylabel('Freqs in Hz')
+    ax2.set_xlabel('Seconds')
