3a24e5e2d4025bedafe2cc9eb25ae542c3094ba3
egochao <johnoldmanworkspace@gmail.com>
2022-08-10 08:44:37 +0000

    update loss

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,5 +1,26 @@
 import torch
 
+from torch import nn
+import torch.nn.functional as F
+
+def student_loss(student_preds, labels):
+    return F.cross_entropy(student_preds, labels)
+
+def kd_loss(student_preds, teacher_preds, T):
+    loss = nn.KLDivLoss(reduction="batchmean", log_target=True)
+
+    return loss(
+        F.log_softmax(student_preds/T, dim=1),
+        F.log_softmax(teacher_preds/T, dim=1),
+    ) * T * T
+
+def distillation_loss(student_preds, teacher_preds, labels, alpha, T):
+    student_loss = student_loss(student_preds, labels)
+    kd_loss = kd_loss(student_preds, teacher_preds, T)
+
+    return kd_loss * alpha + student_loss * (1 - alpha)
+
+
 
 def get_loader_params():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
