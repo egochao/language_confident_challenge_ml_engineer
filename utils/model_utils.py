7766c5525e33eeb9aac7b1b6e8a77dc1a2f3bb96
egochao <johnoldmanworkspace@gmail.com>
2022-08-12 08:46:22 +0000

    clean unuse func

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,51 +1,5 @@
-import torch
-
 from torch import nn
 import torch.nn.functional as F
-import pytorch_lightning as pl
-
-# your favorite machine learning tracking tool
-from pytorch_lightning.loggers import WandbLogger
-
-import torch
-from torch import nn
-from torch.nn import functional as F
-from torch.utils.data import random_split, DataLoader
-
-from torchmetrics import Accuracy
-
-from torchvision import transforms
-from torchvision.datasets import CIFAR10
-
-import wandb
-
-
-class ImagePredictionLogger(pl.callbacks.Callback):
-    def __init__(self, val_samples, num_samples=32):
-        super().__init__()
-        self.num_samples = num_samples
-        self.val_imgs, self.val_labels = val_samples
-
-    def on_validation_epoch_end(self, trainer, pl_module):
-        # Bring the tensors to CPU
-        val_imgs = self.val_imgs.to(device=pl_module.device)
-        val_labels = self.val_labels.to(device=pl_module.device)
-        # Get model prediction
-        logits = pl_module(val_imgs)
-        preds = torch.argmax(logits, -1)
-        # Log the images as wandb Image
-        trainer.logger.experiment.log(
-            {
-                "examples": [
-                    wandb.Image(x, caption=f"Pred:{pred}, Label:{y}")
-                    for x, pred, y in zip(
-                        val_imgs[: self.num_samples],
-                        preds[: self.num_samples],
-                        val_labels[: self.num_samples],
-                    )
-                ]
-            }
-        )
 
 
 def student_loss(student_preds, labels):
